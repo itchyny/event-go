5ba15a877784973c13808d12fbacbad9646a86d2
itchyny <itchyny@hatena.ne.jp>
2021-04-17 14:00:41 +0900

    define interfaces of event, subscriber, and publisher

--- event.go
+++ event.go
@@ -1,1 +1,27 @@
 package event
+
+import "context"
+
+// Type is the event type. The underlying type is int to define nonduplicate
+// event types with iota and to quickly selecting the subscribers on a new event.
+type Type int
+
+// Event is the interface for an event. The Type() method is used to
+// select the registered subscribers.
+type Event interface {
+	Type() Type
+}
+
+// Subscriber is the interface for an event subscriber.
+type Subscriber interface {
+	// Handle an event.
+	Handle(context.Context, Event) error
+}
+
+// Publisher is the interface for an event publisher.
+type Publisher interface {
+	// A publisher is a subscriber.
+	Subscriber
+	// Publish an event.
+	Publish(context.Context, Event) error
+}
